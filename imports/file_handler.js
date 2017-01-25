2c8aa4685a089a77e82c05ffa3f8bffd91693007
Andriux <andriux959@gmail.com>
2017-01-25 15:01:53 +0000

    Implement adding sticker
    File handling unimplemented

--- imports/file_handler.js
+++ imports/file_handler.js
@@ -1,3 +1,6 @@
+/** Returns URL of image
+ *  from user and tags
+ */
 function findImage(user, tags) {
 
     //TODO: implement
@@ -5,4 +8,20 @@ function findImage(user, tags) {
 
 }
 
+/** Saves URL of image
+ *  with user and tags
+ */
+function saveImage(user, tags, image) {
+
+    //TODO: implement
+    return;
+
+}
+
+
+
+
+
+
 module.exports.findImage = findImage;
+module.exports.saveImage = saveImage;
--- imports/prompts.js
+++ imports/prompts.js
@@ -15,6 +15,7 @@ function setupPrompts(client) {
             + "#" + message.author.discriminator 
             + " <" + message.author.id + ">");
         console.log("[MSG]".bgGreen + " Message contents: " + contents);
+        console.log("[MSG]".bgGreen + " Message channel: " + message.channel + " (" + message.channel.type + ")");
 
         /* Checks if '!sticker' */
         var get_sticker_regex = new RegExp("(!sticker) (.*)");
@@ -26,8 +27,6 @@ function setupPrompts(client) {
             /* Gets tags */
             var tags = get_sticker_regex.exec(contents)[2];
 
-            console.log("[CMD]".bgMagenta + " !sticker called, user: " + user.id + ",  tags: " + tags);
-
             /* Finds image */
             var image = file_handler.findImage(user, tags);
             
@@ -36,19 +35,35 @@ function setupPrompts(client) {
                 if (err) console.log(err);
             });
 
+            console.log("[CMD]".bgMagenta + " !sticker called, user: " + user.id + ",  tags: " + tags);
+
         }
 
+        /* Checks if (image attachment exists) and (in direct message) and (not from self) */
+        else if (message.attachments.array().length != 0 && (message.channel.type == "dm" && message.author != client.user)) {
+            
+            /* Gets user */
+            var user = message.author;
 
-    
+            /* Gets tags */
+            var tags = contents;
+
+            /* Gets image attachment url */
+            var image = message.attachments.first().url;
+
+            /* Saves image to database */
+            file_handler.saveImage(user, tags, image);
+
+            console.log("[CMD]".bgMagenta + " !addsticker called, user: " + user.id + ", tags: \'" + tags + "\', url: " + image);
+
+        }
+
+    /* Stock response */
     if (message.content === 'ping') {
         message.reply('pong');
     }
+
     });
-
-    
-
-
-
 
 }
 
